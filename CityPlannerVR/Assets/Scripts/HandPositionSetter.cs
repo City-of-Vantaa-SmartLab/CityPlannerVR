3775144550f9a3f5124bff606baaa925918bec34
Markus Kohonen <mage_ege@hotmail.com>
2017-10-26 10:29:34 +0300

    Merge multiplayer-basic branch to master

---
+++ CityPlannerVR/Assets/Scripts/HandPositionSetter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.VR;
+
+public class HandPositionSetter : NetworkBehaviour
+{
+
+    IEnumerator TrackNodeCoroutine(VRNode node)
+    {
+        while (true)
+        {
+            transform.rotation = InputTracking.GetLocalRotation(node);
+            transform.position = InputTracking.GetLocalPosition(node);
+            yield return null;
+        }
+    }
+
+    [TargetRpc]
+    public void TargetSetHand(NetworkConnection target, VRNode node)
+    {
+        StartCoroutine(TrackNodeCoroutine(node));
+        Debug.Log("RpcSetHand");
+    }
+}
