efa4465024dca670673a51ef7df4aef79f3ad019
Markus Kohonen <mage_ege@hotmail.com>
2017-11-07 14:21:29 +0200

    Testing bugfix in multiplayer

--- CityPlannerVR/Assets/Scripts/InteractableObjectAuthorityHandler.cs
+++ CityPlannerVR/Assets/Scripts/InteractableObjectAuthorityHandler.cs
@@ -5,13 +5,36 @@ using UnityEngine.Networking;
 
 public class InteractableObjectAuthorityHandler : NetworkBehaviour
 {
-
     private void OnAttachedToHand(Valve.VR.InteractionSystem.Hand hand)
     {
         Debug.Log("InteractableObjectAuthorityHandler::OnGrab: Grabbed!");
-        var player = GameObject.FindGameObjectWithTag("VRLocalPlayer");
+        var player = FindLocalPlayer();
         var playerID = player.GetComponent<NetworkIdentity>();
 
         player.GetComponent<PlayerAvatar>().SetAuthForObj(netId, playerID);
+    }
+
+    private GameObject FindLocalPlayer()
+    {
+        GameObject localPlayer = null;
+
+        GameObject[] players = GameObject.FindGameObjectsWithTag("VRLocalPlayer");
+        Debug.Log("Players found: " + players.Length);
+
+        foreach (GameObject player in players)
+        {
+            if(player.GetComponent<NetworkIdentity>().isLocalPlayer)
+            {
+                localPlayer = player;
+                Debug.Log("Local player found!");
+            }
+        }
+
+        if(localPlayer == null)
+        {
+            Debug.LogError("InteractableObjectAuthorityHandler::FindLocalPlayer: Could not find local player!");
+        }
+
+        return localPlayer;
     }
 }
