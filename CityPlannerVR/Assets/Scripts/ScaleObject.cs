0e06144a3553f8921c47d7cb2e85958409009b6a
Markus Kohonen <mage_ege@hotmail.com>
2017-12-01 12:29:03 +0200

    Add player shrinking and teleporting to "real life" city level
    - Add player scaling
    - Add player teleporting
    - Add ability to modify teleport graphic properties as needed
    
    - Does not yet work with networked player avatars

---
+++ CityPlannerVR/Assets/Scripts/ScaleObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Scales the player object when player lets go of this object.
+/// </summary>
+
+public class ScaleObject : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject objectToScale;
+
+    [SerializeField]
+    private Vector3 newScale;
+
+    //private void OnDetachedFromHand(Valve.VR.InteractionSystem.Hand hand)
+    //{
+    //    // Scale player
+    //    Debug.Log("ScalePlayer::OnDetachedFromHand: Scaling");
+    //}
+
+    public void Scale()
+    {
+        Debug.Log("ScalePlayer::Scale: Scaling");
+        objectToScale.transform.localScale = newScale;
+    }
+}
