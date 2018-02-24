89771842ecae3eaa11fe95f52ba6a13595db6516
Taru Lahtinen <taru.lahtinen@iki.fi>
2018-02-24 13:20:12 +0200

    Bugfixes added to binary fromatter version

--- CityPlannerVR/Assets/Scripts/SaveAndLoadObjects.cs
+++ CityPlannerVR/Assets/Scripts/SaveAndLoadObjects.cs
@@ -25,21 +25,19 @@ public class SaveAndLoadObjects : MonoBehaviour {
     public Vector3 objectScale;
 
     private string pathName;
-    private string xmlPathName;
+    //private string xmlPathName;
 
     private void Awake()
     {
         pathName = Application.persistentDataPath + "/ObjectData.dat";
-        xmlPathName = Application.persistentDataPath + "/XmlData.dat";
+        //xmlPathName = Application.persistentDataPath + "/XmlData.dat";
 
-        Debug.Log(pathName);
-
-        XmlLoadObject();
+        LoadObject();
     }
 
     private void OnApplicationQuit()
     {
-        XmlSaveObject();
+        SaveObject();
     }
 
     //This will be the parent of the parent (the table we are going to put all the buildings and other stuff)
@@ -48,87 +46,87 @@ public class SaveAndLoadObjects : MonoBehaviour {
     [HideInInspector]
     public ObjectDatabase dataDB;
 
-    #region Xml debug
-    public void XmlSaveObject()
-    {
-        //Delete the old savefile if there is one
-        if (File.Exists(xmlPathName))
-        {
-            File.Delete(xmlPathName);
-        }
+    //#region Xml debug
+    //public void XmlSaveObject()
+    //{
+    //    //Delete the old savefile if there is one
+    //    if (File.Exists(xmlPathName))
+    //    {
+    //        File.Delete(xmlPathName);
+    //    }
 
-        XmlSerializer serializer = new XmlSerializer(typeof(ObjectDatabase));
-        FileStream file = File.Create(xmlPathName);
+    //    XmlSerializer serializer = new XmlSerializer(typeof(ObjectDatabase));
+    //    FileStream file = File.Create(xmlPathName);
 
-        for (int i = 0; i < ObjectContainer.objects.Count; i++)
-        {
+    //    for (int i = 0; i < ObjectContainer.objects.Count; i++)
+    //    {
 
-            dataDB.list.Add(new ObjectData());
-            dataDB.list[i].objectName = ObjectContainer.objects[i].name;
+    //        dataDB.list.Add(new ObjectData());
+    //        dataDB.list[i].objectName = ObjectContainer.objects[i].name;
 
-            //Vector3
-            dataDB.list[i].objectPosition[0] = ObjectContainer.objects[i].transform.localPosition.x;
-            dataDB.list[i].objectPosition[1] = ObjectContainer.objects[i].transform.localPosition.y;
-            dataDB.list[i].objectPosition[2] = ObjectContainer.objects[i].transform.localPosition.z;
+    //        //Vector3
+    //        dataDB.list[i].objectPosition[0] = ObjectContainer.objects[i].transform.localPosition.x;
+    //        dataDB.list[i].objectPosition[1] = ObjectContainer.objects[i].transform.localPosition.y;
+    //        dataDB.list[i].objectPosition[2] = ObjectContainer.objects[i].transform.localPosition.z;
 
-            //Quaternion
-            dataDB.list[i].objectRotation[0] = ObjectContainer.objects[i].transform.localRotation.x;
-            dataDB.list[i].objectRotation[1] = ObjectContainer.objects[i].transform.localRotation.y;
-            dataDB.list[i].objectRotation[2] = ObjectContainer.objects[i].transform.localRotation.z;
-            dataDB.list[i].objectRotation[3] = ObjectContainer.objects[i].transform.localRotation.w;
+    //        //Quaternion
+    //        dataDB.list[i].objectRotation[0] = ObjectContainer.objects[i].transform.localRotation.x;
+    //        dataDB.list[i].objectRotation[1] = ObjectContainer.objects[i].transform.localRotation.y;
+    //        dataDB.list[i].objectRotation[2] = ObjectContainer.objects[i].transform.localRotation.z;
+    //        dataDB.list[i].objectRotation[3] = ObjectContainer.objects[i].transform.localRotation.w;
 
-            //Vector3
-            dataDB.list[i].objectScale[0] = ObjectContainer.objects[i].transform.localScale.x;
-            dataDB.list[i].objectScale[1] = ObjectContainer.objects[i].transform.localScale.y;
-            dataDB.list[i].objectScale[2] = ObjectContainer.objects[i].transform.localScale.z;
-        }
+    //        //Vector3
+    //        dataDB.list[i].objectScale[0] = ObjectContainer.objects[i].transform.localScale.x;
+    //        dataDB.list[i].objectScale[1] = ObjectContainer.objects[i].transform.localScale.y;
+    //        dataDB.list[i].objectScale[2] = ObjectContainer.objects[i].transform.localScale.z;
+    //    }
 
-        serializer.Serialize(file, dataDB);
-        file.Close();
-    }
+    //    serializer.Serialize(file, dataDB);
+    //    file.Close();
+    //}
 
-    public void XmlLoadObject()
-    {
-        if (File.Exists(xmlPathName))
-        {
-            XmlSerializer serializer = new XmlSerializer(typeof(ObjectDatabase));
-            FileStream file = File.Open(xmlPathName, FileMode.Open);
-            if (file.Length > 0)
-            {
-                dataDB = (ObjectDatabase)serializer.Deserialize(file);
-                file.Close();
+    //public void XmlLoadObject()
+    //{
+    //    if (File.Exists(xmlPathName))
+    //    {
+    //        XmlSerializer serializer = new XmlSerializer(typeof(ObjectDatabase));
+    //        FileStream file = File.Open(xmlPathName, FileMode.Open);
+    //        if (file.Length > 0)
+    //        {
+    //            dataDB = (ObjectDatabase)serializer.Deserialize(file);
+    //            file.Close();
 
-                GameObject parent = new GameObject
-                {
-                    name = "Parent"
-                };
-                //Hardcoded for now
-                parent.transform.parent = table.transform;
-                parent.transform.localPosition = new Vector3(0, 0, 0);
-                parent.transform.localRotation = Quaternion.identity;
-                parent.transform.localScale = new Vector3(1, 1, 1);
+    //            GameObject parent = new GameObject
+    //            {
+    //                name = "Parent"
+    //            };
+    //            //Hardcoded for now
+    //            parent.transform.parent = table.transform;
+    //            parent.transform.localPosition = new Vector3(0, 0, 0);
+    //            parent.transform.localRotation = Quaternion.identity;
+    //            parent.transform.localScale = new Vector3(1, 1, 1);
 
-                for (int i = 0; i < dataDB.list.Count; ++i)
-                {
-                    objectName = dataDB.list[i].objectName;
-                    objectPosition = new Vector3(dataDB.list[i].objectPosition[0], dataDB.list[i].objectPosition[1], dataDB.list[i].objectPosition[2]);
-                    objectRotation = new Quaternion(dataDB.list[i].objectRotation[0], dataDB.list[i].objectRotation[1], dataDB.list[i].objectRotation[2], dataDB.list[i].objectRotation[3]);
-                    objectScale = new Vector3(dataDB.list[i].objectScale[0], dataDB.list[i].objectScale[1], dataDB.list[i].objectScale[2]);
+    //            for (int i = 0; i < dataDB.list.Count; ++i)
+    //            {
+    //                objectName = dataDB.list[i].objectName;
+    //                objectPosition = new Vector3(dataDB.list[i].objectPosition[0], dataDB.list[i].objectPosition[1], dataDB.list[i].objectPosition[2]);
+    //                objectRotation = new Quaternion(dataDB.list[i].objectRotation[0], dataDB.list[i].objectRotation[1], dataDB.list[i].objectRotation[2], dataDB.list[i].objectRotation[3]);
+    //                objectScale = new Vector3(dataDB.list[i].objectScale[0], dataDB.list[i].objectScale[1], dataDB.list[i].objectScale[2]);
 
-                    GameObject go = Instantiate(Resources.Load("Prefabs/Buildings/" + objectName, typeof(GameObject))) as GameObject;
-                    go.name = objectName;
-                    go.transform.parent = parent.transform;
-                    go.transform.localPosition = objectPosition;
-                    go.transform.rotation = objectRotation;
-                    go.transform.localScale = objectScale;
-                }
+    //                GameObject go = Instantiate(Resources.Load("Prefabs/Buildings/" + objectName, typeof(GameObject))) as GameObject;
+    //                go.name = objectName;
+    //                go.transform.parent = parent.transform;
+    //                go.transform.localPosition = objectPosition;
+    //                go.transform.rotation = objectRotation;
+    //                go.transform.localScale = objectScale;
+    //            }
 
-                InitializeObjectDatabase();
+    //            InitializeObjectDatabase();
 
-            }
-        }
-    }
-    #endregion
+    //        }
+    //    }
+    //}
+    //#endregion
 
 
     void InitializeObjectDatabase()
@@ -142,9 +140,14 @@ public class SaveAndLoadObjects : MonoBehaviour {
     #region save and load
     public void SaveObject()
     {
+        //Delete the old savefile if there is one
+        if (File.Exists(pathName))
+        {
+            File.Delete(pathName);
+        }
+
         BinaryFormatter bf = new BinaryFormatter();
         FileStream file = File.Create(pathName);
-        //ObjectDatabase dataDB = new ObjectDatabase();
 
         for (int i = 0; i < ObjectContainer.objects.Count; i++)
         {
@@ -193,22 +196,22 @@ public class SaveAndLoadObjects : MonoBehaviour {
                 parent.transform.localRotation = Quaternion.identity;
                 parent.transform.localScale = new Vector3(1, 1, 1);
 
-                //All the lists are (or should be) the same lenght
-                for (int i = 0; i < dataDB.list.Count; i++)
+                for (int i = 0; i < dataDB.list.Count; ++i)
                 {
                     objectName = dataDB.list[i].objectName;
                     objectPosition = new Vector3(dataDB.list[i].objectPosition[0], dataDB.list[i].objectPosition[1], dataDB.list[i].objectPosition[2]);
                     objectRotation = new Quaternion(dataDB.list[i].objectRotation[0], dataDB.list[i].objectRotation[1], dataDB.list[i].objectRotation[2], dataDB.list[i].objectRotation[3]);
                     objectScale = new Vector3(dataDB.list[i].objectScale[0], dataDB.list[i].objectScale[1], dataDB.list[i].objectScale[2]);
 
-                    dataDB.list.Remove(dataDB.list[i]);
-
                     GameObject go = Instantiate(Resources.Load("Prefabs/Buildings/" + objectName, typeof(GameObject))) as GameObject;
-                    go.transform.position = objectPosition;
+                    go.name = objectName;
+                    go.transform.parent = parent.transform;
+                    go.transform.localPosition = objectPosition;
                     go.transform.rotation = objectRotation;
                     go.transform.localScale = objectScale;
-                    //go.transform.parent = parent.transform;
                 }
+
+                InitializeObjectDatabase();
             }
         }
     }
