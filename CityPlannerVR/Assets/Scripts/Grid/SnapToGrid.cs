e0a5b1a4e409dfa498c7f34dd9e8af6adb9a56c0
Temp <Temp>
2018-02-28 09:15:56 +0200

    Prisma disabled for the demo event

--- CityPlannerVR/Assets/Scripts/Grid/SnapToGrid.cs
+++ CityPlannerVR/Assets/Scripts/Grid/SnapToGrid.cs
@@ -81,7 +81,7 @@ public class SnapToGrid : MonoBehaviour {
         {
 			//Moves the object to the grids position									    just a bit higher than the table, so the object collider won't go inside a table collider
 			transform.position = new Vector3 (hit.collider.gameObject.transform.position.x, hit.collider.gameObject.transform.position.y * 1.3f, hit.collider.gameObject.transform.position.z);
-            transform.parent = parent.transform;
+            //transform.parent = parent.transform;
 
 			CheckRotation();
         }
