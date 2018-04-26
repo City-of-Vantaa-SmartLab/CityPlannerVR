49fe38556cdfee405c780dcb8522d27088fa323f
Temp <Temp>
2018-04-26 15:45:02 +0300

    Move and remove points added, scaling bug fix attempted

--- CityPlannerVR/Assets/Scripts/CameraTool/CameraPathHandler.cs
+++ CityPlannerVR/Assets/Scripts/CameraTool/CameraPathHandler.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class CameraPathHandler : MonoBehaviour {
 
+	//The gameobject to be instantiated
     public GameObject pathPoint;
+
+	public GameObject videoCamera;
+
+	public bool cameraStart = false;
 
 #region private variables
 
@@ -14,12 +19,19 @@ public class CameraPathHandler : MonoBehaviour {
 	InputListener inputListener;
 	XRLineRenderer line;
 
+	//The instantiated gameobject
 	GameObject point;
 
 	//Put all the points on the path here
 	List<GameObject> pathPoints;
 
-	int i = 0;
+	int pathPointIndex = 0;
+
+	GameObject selectedPoint;
+	bool holdTrigger = false;
+
+
+	float cameraSpeed = 1;
 
 #endregion
 
@@ -32,14 +44,26 @@ public class CameraPathHandler : MonoBehaviour {
 		InitializePathLine ();
 		tool = Tool.Add;
 
+		videoCamera.SetActive (false);
+
 		Subscribe ();
 	}
 
 	void Subscribe(){
 	
+		inputListener.TriggerClicked += TriggerPressed;
+		inputListener.TriggerLifted += TriggerReleased;
+
 		inputListener.TriggerClicked += InstantiatePathPoint;
+		inputListener.TriggerClicked += ActivatePointMoving;
+		inputListener.TriggerClicked += RemovePoint;
+
+		inputListener.TriggerClicked += jokuKamera;
 	}
 
+	//--------------------------------------------------------------------------------------------------------------------------------
+	//													INSTANTIATE POINT
+	//--------------------------------------------------------------------------------------------------------------------------------
 
 	private void InstantiatePathPoint(object sender, ClickedEventArgs e)
     {
@@ -53,21 +77,134 @@ public class CameraPathHandler : MonoBehaviour {
 
 	private void DrawLineBetweenPoints(){
 
-		line.SetVertexCount (i + 1);
-		line.SetPosition (i, pathPoints[i].transform.position);
-		i++;
+		line.SetVertexCount (pathPointIndex + 1);
+		line.SetPosition (pathPointIndex, pathPoints[pathPointIndex].transform.position);
+		pathPointIndex++;
 		
 	}
 
 	private void InitializePathLine(){
 		line.SetVertexCount (0);
+		pathPointIndex = 0;
 	}
 
-	private void MovePoint(){
-		
+	//--------------------------------------------------------------------------------------------------------------------------------
+	//													MOVE POINT
+	//--------------------------------------------------------------------------------------------------------------------------------
+	private void ActivatePointMoving(object sender, ClickedEventArgs e){
+		if (tool == Tool.Move) {
+			StartCoroutine (MovePoint ());
+		}
 	}
 
-	private void RemovePoint(){
+	private IEnumerator MovePoint(){
+
+		if (selectedPoint != null) {
+
+			while (holdTrigger) {
+				selectedPoint.transform.position = transform.position;
+				selectedPoint.transform.rotation = transform.rotation;
+
+				int index = pathPoints.IndexOf (selectedPoint);
+				line.SetPosition (index, selectedPoint.transform.position);
+
+				yield return null;
+			}
+		}
+
+		else {
+			yield break;
+		}
+	}
+
+	private void TriggerPressed(object sender, ClickedEventArgs e){
+		holdTrigger = true;
+	}
+
+	private void TriggerReleased(object sender, ClickedEventArgs e){
+		holdTrigger = false;
+	}
+
+	//--------------------------------------------------------------------------------------------------------------------------------
+	//													REMOVE POINT
+	//--------------------------------------------------------------------------------------------------------------------------------
+
+	private void RemovePoint(object sender, ClickedEventArgs e){
+		if (tool == Tool.Remove) {
+			if (selectedPoint != null) {
+
+				pathPoints.Remove (selectedPoint);
+
+				GameObject.Destroy (selectedPoint);
+
+				ReDrawPath ();
+
+				selectedPoint = null;
+				tool = Tool.Add;
+			}
+		}
+	}
+
+	private void ReDrawPath(){
+
+		InitializePathLine ();
+		line.SetVertexCount (pathPoints.Count);
+		pathPointIndex = pathPoints.Count;
+
+		for (int i = 0; i < pathPoints.Count; i++) {
+			line.SetPosition(i, pathPoints[i].transform.position);
+		}
+	}
+
+	//--------------------------------------------------------------------------------------------------------------------------------
+	//													CAMERA
+	//--------------------------------------------------------------------------------------------------------------------------------
+
+	private void jokuKamera(object sender, ClickedEventArgs e){
+
+		if (cameraStart) {
+			
+			videoCamera.SetActive (true);
+			videoCamera.transform.position = pathPoints [0].transform.position;
+			videoCamera.transform.rotation = pathPoints [0].transform.rotation;
+
+			//StartCoroutine (MoveCamera ());
+		}
+	}
+
+	private IEnumerator MoveCamera(){
+		//Camera starts at 0 and its first target is 1
+		int targetIndex = 1;
+
+		Vector3 targetPoint = pathPoints[targetIndex].transform.position;
+		//while the position of the videoCamera is not the same as the last points position we want to move the camera
+		while (videoCamera.transform.position != pathPoints[pathPoints.Count - 1].transform.position){
+
+			videoCamera.transform.position = targetPoint * Time.deltaTime * cameraSpeed;
+			if (Vector3.Distance(videoCamera.transform.position, targetPoint) >= 0.1f) {
+				targetIndex++;
+			}
+
+			yield return null;
+		}
+		yield break;
+	}
 		
+	//--------------------------------------------------------------------------------------------------------------------------------
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "CameraPathPoint") {
+			selectedPoint = other.gameObject;
+
+			tool = Tool.Move;
+		}
+	}
+
+	void OnCollisionExit(Collision other){
+		if (other.gameObject.tag == "CameraPathPoint") {
+			selectedPoint = null;
+
+			tool = Tool.Add;
+		}
 	}
 }
