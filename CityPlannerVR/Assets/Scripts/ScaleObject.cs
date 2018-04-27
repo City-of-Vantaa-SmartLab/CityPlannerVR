9ae462efbd14983c29f10a42afaffb72a5126dfe
Temp <Temp>
2018-04-27 10:17:10 +0300

    Scale bug fix attempt 4

--- CityPlannerVR/Assets/Scripts/ScaleObject.cs
+++ CityPlannerVR/Assets/Scripts/ScaleObject.cs
@@ -47,9 +47,8 @@ public class ScaleObject : MonoBehaviour {
             PhotonView photonView;
             photonView = pa.GetComponent<PhotonView>();
 
-            //photonView.RPC("SetScale", PhotonTargets.All, newScale);
-            pa.SetScale(newScale);
-            photonView.RPC("UpdateScale", PhotonTargets.All);
+            pa.UpdateScale(newScale);
+            //photonView.RPC("UpdateScale", PhotonTargets.All);
         }
 
         else
