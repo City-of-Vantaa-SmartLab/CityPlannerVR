824d32af9e64e1f728a49d5a768e95278a53f828
Unknown <asser.salmela@gmail.com>
2018-06-04 16:06:30 +0300

    Added functionality to ScrollbarManager
    Scroll rect's values can be modified with customized functions

--- CityPlannerVR/Assets/Scripts/UIandTools/ScrollbarManager.cs
+++ CityPlannerVR/Assets/Scripts/UIandTools/ScrollbarManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 
 /// <summary>
 /// Attach this script to a gameobject with a misbehaving scroll rect. Assign scrollbars as needed.
+/// If not using scrollbars use Scroll*Direction* functions instead.
 /// </summary>
 
 public class ScrollbarManager : MonoBehaviour {
@@ -16,8 +17,8 @@ public class ScrollbarManager : MonoBehaviour {
     private float xValue;
     private float yValue;
 
-    //public delegate void EventWithCoordinates(float xCoord, float yCoord);
-    //public event EventWithCoordinates UpdateSliderCoordinates;
+    public delegate void EventWithCoordinates(float xCoord, float yCoord);
+    public event EventWithCoordinates UpdateSliderCoordinates;
     //public event EventWithComment ChangeCurrentComment;
 
     private void Start()
@@ -27,44 +28,64 @@ public class ScrollbarManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        if (horizontalScrollbar)
+        if (scrollRect)
         {
-            xValue = horizontalScrollbar.value;
-            scrollRect.horizontalNormalizedPosition = xValue;
-        }
-        if (verticalScrollbar)
-        {
-            yValue = verticalScrollbar.value;
-            scrollRect.verticalNormalizedPosition = yValue;
+            if (horizontalScrollbar)
+            {
+                xValue = horizontalScrollbar.value;
+                scrollRect.horizontalNormalizedPosition = xValue;
+            }
+            if (verticalScrollbar)
+            {
+                yValue = verticalScrollbar.value;
+                scrollRect.verticalNormalizedPosition = yValue;
+            }
         }
     }
 
-    //public void ScrollUp(float increment)
-    //{
-    //    xValue += increment;
-    //    OnSliderUpdate(xValue, yValue);
-    //}
+    public void ScrollRight(float increment)
+    {
+        if (xValue + increment > 1)
+            xValue = 1;
+        else
+            xValue += increment;
+        OnSliderUpdate(xValue, yValue);
+    }
 
-    //public void ScrollDown(float increment)
-    //{
-    //    xValue -= increment;
-    //    OnSliderUpdate(xValue, yValue);
-    //}
-    //public void ScrollLeft(float increment)
-    //{
-    //    yValue -= increment;
-    //    OnSliderUpdate(xValue, yValue);
-    //}
-    //public void ScrollRight(float increment)
-    //{
-    //    yValue += increment;
-    //    OnSliderUpdate(xValue, yValue);
-    //}
+    public void ScrollLeft(float increment)
+    {
+        if (xValue - increment < 0)
+            xValue = 0;
+        else
+            xValue -= increment;
+        OnSliderUpdate(xValue, yValue);
+    }
+    public void ScrollDown(float increment)
+    {
+        if (yValue - increment < 0)
+            yValue = 0;
+        else
+            yValue -= increment;
+        OnSliderUpdate(xValue, yValue);
+    }
+    public void ScrollUp(float increment)
+    {
+        if (yValue + increment > 1)
+            yValue = 1;
+        else
+            yValue += increment;
+        OnSliderUpdate(xValue, yValue);
+    }
 
-    //private void OnSliderUpdate(float xCoord, float yCoord)
-    //{
-    //    if (UpdateSliderCoordinates != null)
-    //        UpdateSliderCoordinates(xCoord, yCoord);
-    //}
+    public void OnSliderUpdate(float xValue, float yValue)
+    {
+        if (UpdateSliderCoordinates != null)
+            UpdateSliderCoordinates(xValue, yValue);
+        if (scrollRect)
+        {
+            scrollRect.horizontalNormalizedPosition = xValue;
+            scrollRect.verticalNormalizedPosition = yValue;
+        }
+    }
 
 }
