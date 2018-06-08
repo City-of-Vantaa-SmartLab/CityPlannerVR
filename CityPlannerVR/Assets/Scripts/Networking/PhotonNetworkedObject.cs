6324ec06084f8856fbe107e1cb8fea5a5a277c62
Santeri Saarinen <ss.deadweight@gmail.com>
2018-06-08 10:55:12 +0300

    Networked object changed

--- CityPlannerVR/Assets/Scripts/Networking/PhotonNetworkedObject.cs
+++ CityPlannerVR/Assets/Scripts/Networking/PhotonNetworkedObject.cs
@@ -10,15 +10,34 @@ public class PhotonNetworkedObject : Photon.MonoBehaviour {
 	private Vector3 correctObjectPos = Vector3.zero;
 	private Quaternion correctObjectRot = Quaternion.identity;
 
+	private bool isInHand = false;
+	private IsAttachedToHand isAttachedToHand = null;
+
 	#endregion
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (!photonView.isMine)
-		{
-			transform.position = Vector3.Lerp(transform.position, this.correctObjectPos, Time.deltaTime * 5);
-			transform.rotation = Quaternion.Lerp(transform.rotation, this.correctObjectRot, Time.deltaTime * 5);
+		if (isAttachedToHand == null) {
+			isAttachedToHand = this.gameObject.GetComponent<IsAttachedToHand> ();
+		}
+
+		if (!photonView.isMine) {
+			transform.position = Vector3.Lerp (transform.position, this.correctObjectPos, Time.deltaTime * 5);
+			transform.rotation = Quaternion.Lerp (transform.rotation, this.correctObjectRot, Time.deltaTime * 5);
+		} else {
+			if (isAttachedToHand.IsHolding && !isInHand) {
+				isInHand = true;
+			} else if(!isAttachedToHand.IsHolding && isInHand){
+				isInHand = false;
+			}
+		}
+
+		if (isInHand && this.gameObject.GetComponent<Rigidbody>().useGravity) {
+			this.gameObject.GetComponent<Rigidbody> ().useGravity = false;
+		}
+		if(!isInHand && !this.gameObject.GetComponent<Rigidbody>().useGravity) {
+			this.gameObject.GetComponent<Rigidbody> ().useGravity = true;
 		}
 	}
 
