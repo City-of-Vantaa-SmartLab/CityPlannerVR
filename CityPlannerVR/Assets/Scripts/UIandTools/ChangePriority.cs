dc948dfda4e71bbb0d3a61e731317136a20eef4c
Temp <Temp>
2018-07-06 15:11:58 +0300

    Deleted some old buildings

--- CityPlannerVR/Assets/Scripts/UIandTools/ChangePriority.cs
+++ CityPlannerVR/Assets/Scripts/UIandTools/ChangePriority.cs
@@ -10,6 +10,9 @@ public class ChangePriority : MonoBehaviour {
     InputMaster inputMaster;
     Dissonance.VoiceBroadcastTrigger voiceTrigger;
 
+    /// <summary>
+    /// Get InputMaster and VoiceBroadcastTrigger every time tis object is picked up
+    /// </summary>
     public void GetInputMaster()
     {
         inputMaster = GetComponentInParent<InputMaster>();
@@ -21,6 +24,7 @@ public class ChangePriority : MonoBehaviour {
     /// </summary>
     public void ChangeSpeakingPriority()
     {
+        //If the player holding this object is admin
         if (inputMaster.Role == InputMaster.RoleType.Admin)
         {
             if(voiceTrigger.Priority == Dissonance.ChannelPriority.None)
