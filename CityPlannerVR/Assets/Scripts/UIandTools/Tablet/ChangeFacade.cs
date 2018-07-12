95e2c303b1d4ade49c7b4209cb64e848ea2130f8
Temp <Temp>
2018-07-12 09:56:45 +0300

    Building facade change started

---
+++ CityPlannerVR/Assets/Scripts/UIandTools/Tablet/ChangeFacade.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeFacade : MonoBehaviour {
+
+    [Tooltip("List of all the possible facades for this object")]
+    public GameObject[] facades;
+    int facadeIndex = 0;
+    /// <summary>
+    /// The facade that needs to be disabled when new one is activated (the old facade)
+    /// </summary>
+    GameObject previousFacade;
+
+    private void Start()
+    {
+        previousFacade = facades[facadeIndex];
+        foreach (var facade in facades)
+        {
+            facade.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Used to scroll facades
+    /// </summary>
+    public void ScrollFacades()
+    {
+        if (facades.Length > 0)
+        {
+            if (facadeIndex == facades.Length - 1)
+            {
+                facadeIndex = 0;
+            }
+            else
+            {
+                ++facadeIndex;
+            }
+        }
+    }
+
+    void FacadeChange()
+    {
+        previousFacade.SetActive(false);
+        facades[facadeIndex].SetActive(true);
+        previousFacade = facades[facadeIndex];
+    }
+}
---
+++ CityPlannerVR/Assets/Scripts/UIandTools/Tablet/SetName.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Give this objects name and facades to the tablet
+/// </summary>
+public class SetName : MonoBehaviour {
+
+    /// <summary>
+    /// Name of this object
+    /// </summary>
+    private string objectName;
+    /// <summary>
+    /// Name of this object (read only)
+    /// </summary>
+    public string ObjectName
+    {
+        get
+        {
+            return objectName;
+        }
+    }
+    GameObject[] facades;
+
+    private void Start()
+    {
+        name = gameObject.name;
+        facades = new GameObject[transform.childCount];
+        for (int i = 0; i < facades.Length; i++)
+        {
+            facades[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void GiveNameAndFacades()
+    {
+        
+    }
+}
