82291cb8a35d1baf09ec7f02665b49d1758d204b
Santeri Saarinen <ss.deadweight@gmail.com>
2018-07-13 15:35:02 +0300

    PlayAreaColider added and door interaction stopped

---
+++ CityPlannerVR/Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+public class DoorBehaviour : MonoBehaviour
+{
+
+	private bool isActive = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	public void CloseDoor()
+	{
+		isActive = false;
+		this.gameObject.GetComponent<Interactable> ().enabled = false;
+	}
+}
+
