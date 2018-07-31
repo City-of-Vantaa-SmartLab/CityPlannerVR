d60a25d15c77b006ec337da82e8cd38b6ab1fa3f
Temp <Temp>
2018-07-31 12:37:13 +0300

    Valot prefab added

---
+++ CityPlannerVR/Assets/Scripts/UIandTools/Tablet/ChangePage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePage : MonoBehaviour {
+
+    [Tooltip("The index of the page this button loads")]
+    public int nextPageIndex;
+
+    private Page page;
+    private int previousPage;
+    private int[] nextPages;
+
+    private void Start()
+    {
+        page = GetComponentInParent<Page>();
+        previousPage = page.previousPage;
+        nextPages = page.nextPages;
+    }
+
+    public void ChangePageWhenPressed()
+    {
+        if (nextPages.Contains(nextPageIndex))
+        {
+
+        }
+        else
+        {
+            Debug.LogError("Invalid page index. Check ");
+        }
+    }
+}
