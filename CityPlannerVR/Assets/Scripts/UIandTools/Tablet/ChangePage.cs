20e2c5b321a56f45fe0e28cc9ec4d7874aeecadd
Temp <Temp>
2018-08-01 13:13:44 +0300

    Button hover highlight added

--- CityPlannerVR/Assets/Scripts/UIandTools/Tablet/ChangePage.cs
+++ CityPlannerVR/Assets/Scripts/UIandTools/Tablet/ChangePage.cs
@@ -7,22 +7,35 @@ public class ChangePage : MonoBehaviour {
     [Tooltip("The index of the page this button loads")]
     public int nextPageIndex;
 
+    private HoverTabletManager hoverTablet;
     private Page page;
     private int previousPage;
     private int[] nextPages;
 
     private void Start()
     {
+        hoverTablet = GetComponentInParent<HoverTabletManager>();
         page = GetComponentInParent<Page>();
         previousPage = page.previousPage;
         nextPages = page.nextPages;
     }
 
-    public void ChangePageWhenPressed()
+    /// <summary> Gives the HoverTabletManager the index of the next page when button is pressed </summary>
+    public void ChangePageToNext()
     {
         if (nextPages.Contains(nextPageIndex))
         {
-
+            hoverTablet.PageIndex = nextPageIndex;
         }
+        else
+        {
+            Debug.LogError("Next page index not found");
+        }
+    }
+
+    /// <summary> Gives the HoverTabletManager the index of the previous page when button is pressed </summary>
+    public void ChangePageToPrevious()
+    {
+        hoverTablet.PageIndex = previousPage;
     }
 }
