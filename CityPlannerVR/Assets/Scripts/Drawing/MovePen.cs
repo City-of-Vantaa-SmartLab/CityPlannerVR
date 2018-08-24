459456de355c0c8197c046cc9f7f4eead46e4dd8
Santeri Saarinen <ss.deadweight@gmail.com>
2018-08-24 10:46:30 +0300

    Added drawing prefabs

---
+++ CityPlannerVR/Assets/Scripts/Drawing/MovePen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePen : MonoBehaviour {
+
+    public float speed = 0.5f;
+
+	void Update () {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Time.deltaTime * speed);
+        if (Input.GetKey(KeyCode.RightShift))
+        {
+            transform.Translate(-Vector3.forward * speed * 0.1f);
+        }
+        if (Input.GetKey(KeyCode.RightControl))
+        {
+            transform.Translate(Vector3.forward * speed * 0.1f);
+        }
+    }
+}
