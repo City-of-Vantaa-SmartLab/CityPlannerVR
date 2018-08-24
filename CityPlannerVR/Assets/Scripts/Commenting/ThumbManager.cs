c982613840714ca0c21d4aaa9137fa08cf0fffce
Santeri Saarinen <ss.deadweight@gmail.com>
2018-08-24 13:24:46 +0300

    Thumbs work in UI

--- CityPlannerVR/Assets/Scripts/Commenting/ThumbManager.cs
+++ CityPlannerVR/Assets/Scripts/Commenting/ThumbManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// A test class for thumbs button, will be removed?
@@ -9,67 +10,16 @@ using UnityEngine;
 
 public class ThumbManager : MonoBehaviour {
 
-    private SaveAndLoadComments SLComments;
-    private CommentToolManager commentToolManager;
-    private GameObject player;
-    //private GameObject target;
-    private string commentLayer = "CommentTool";
-
     private void Start()
     {
-        SLComments = GameObject.Find("GameController").GetComponent<SaveAndLoadComments>();
-        commentToolManager = transform.GetComponentInParent<CommentToolManager>();
     }
 
-    public void Test()
-    {
-        Debug.Log("THUMBS MANAGER TESTAA");
-    }
+	public void UpdateThumbText()
+	{
+		int oldNo = int.Parse (this.gameObject.GetComponentInChildren<Text> ().text);
+		int newNo = oldNo + 1;
 
-    //public void CreateThumbUp()
-    //{
-    //    CommentData tempData = CreateThumbData();
-    //    tempData.dataString = "1";
-    //    SLComments.CreateNewComment(tempData);
-    //}
-
-    //public void CreateThumbDown()
-    //{
-    //    CommentData tempData = CreateThumbData();
-    //    tempData.dataString = "0";
-    //    SLComments.CreateNewComment(tempData);
-    //}
-
-    //needs to be cleaned up
-    private CommentData CreateThumbData()
-    {
-        String userName = PhotonNetwork.player.NickName;
-        //string targetName = commentToolManager.LEArgs.target.name;
-
-        CommentData tempData = new CommentData();
-
-        //tempData.userName = PhotonNetwork.player.NickName; //check with PhotonNetwork.player.IsLocal ?
-        //tempData.userName = commenter.GetComponent<PhotonView>().owner.NickName;
-        tempData.userName = userName;
-        tempData.commentedObjectName = commentToolManager.targetName;
-        tempData.SHPath = "";
-        //tempData.commentatorPosition = player.transform.position;
-        tempData.commentType = Comment.CommentType.Thumb;
-        //tempData.type = 2;
-
-        tempData.quickcheck = 0; //will be created in saveAndLoadComments script
-
-        return tempData;
-    }
-
-    //private GameObject GetTarget(object sender, LaserEventArgs e)
-    //{
-    //    if (e.target.gameObject.layer != LayerMask.NameToLayer(commentLayer))
-    //    {
-    //        return e.target.gameObject;
-    //    }
-    //    else
-    //        return null;
-    //}
+		this.gameObject.GetComponentInChildren<Text> ().text = newNo.ToString ();
+	}
 
 }
